fb630b041d516065150a7104ad698037a555e1ff
Tanguy Kervran <t.kervran29@gmail.com>
2022-02-24 17:36:15 -0800

    Add Starship prompt configuration

--- install.sh
+++ install.sh
@@ -16,13 +16,19 @@ nix-env -iA \
 
 git clone https://github.com/oh-my-fish/plugin-foreign-env.git ~/plugin-foreign-env
 
-# stow
-stow git
-stow fish
-stow nvim
-
 # add fish to valid login shells
 command -v fish | sudo tee -a /etc/shells
 
 # use fish as default shell
 sudo chsh -s $(which fish) $USER
+
+curl -fsSL https://starship.rs/install.sh >> starship.sh
+chmod +x starship.sh
+./starship.sh
+rm starship.sh
+
+# stow
+stow git
+stow fish
+stow nvim
+stow starship
