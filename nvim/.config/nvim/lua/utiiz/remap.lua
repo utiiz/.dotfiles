1f08e2c4ea1209aa4f78860ae658fe46c3018c74
Tanguy Kervran <t.kervran29@gmail.com>
2022-10-03 00:43:36 +0200

    ✨ Add Coc config

--- nvim/.config/nvim/lua/utiiz/remap.lua
+++ nvim/.config/nvim/lua/utiiz/remap.lua
@@ -7,7 +7,7 @@ local nmap = require("utiiz.keymap").nmap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 
--- Snippet keymap
+-- Coc keymap
 imap("<TAB>", "<Plug>(coc-snippets-expand)")
 inoremap("<CR>", 'pumvisible() ? "\\<C-y>" : "\\<C-g>u\\<CR>"', {expr = true})
 inoremap("<TAB>", 'pumvisible() ? "\\<C-n>" : "\\<TAB>"', {expr = true})
@@ -15,6 +15,13 @@ inoremap("<S-TAB>", 'pumvisible() ? "\\<C-p>" : "\\<S-TAB>"', {expr = true})
 
 xmap("<leader>a", "<Plug>(coc-codeaction-selected)<CR>")
 nmap("<leader>a", "<Plug>(coc-codeaction-selected)<CR>")
+
+nmap("gd", "<Plug>(coc-definition)", {silent = true})
+nmap("gy", "<Plug>(coc-type-definition)", {silent = true})
+nmap("gi", "<Plug>(coc-implementation)", {silent = true})
+nmap("gr", "<Plug>(coc-references)", {silent = true})
+
+nmap("rn", "<Plug>(coc-rename)", {silent = true})
 
 -- Search
 nnoremap("<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
