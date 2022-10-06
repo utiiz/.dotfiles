f6f83a2cc358a8b9500e1b01e11a4f46adbc2a2f
Tanguy Kervran <t.kervran29@gmail.com>
2022-10-06 07:49:52 +0200

    ✨ ISRT Layout file

---
+++ nvim/.config/nvim/after/plugin/keymaps/isrt_layout.lua
@@ -0,0 +1,9 @@
+local noremap = require("utiiz.keymap").noremap
+
+-- noremap("p", "h")
+-- noremap("n", "j")
+-- noremap("e", "k")
+-- noremap("a", "l")
+-- 
+-- noremap("u", "p")
+-- noremap("z", "u")
