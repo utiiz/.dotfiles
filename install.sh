68dd5214ffa0f527fd2e69d4ffec9b99343cd636
Tanguy Kervran <t.kervran29@gmail.com>
2022-12-02 23:25:01 +0100

    🚧 Add TMUX plugin install

--- install.sh
+++ install.sh
@@ -37,8 +37,9 @@ stow starship
 touch ~/.hushlogin
 
 # Packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
