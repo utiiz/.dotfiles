bf13faa3d447965793c3748235b61a480d16a3c0
Tanguy Kervran <t.kervran29@gmail.com>
2022-12-02 17:59:06 +0100

    Add Packer to install.sh

--- install.sh
+++ install.sh
@@ -36,6 +36,10 @@ stow starship
 
 touch ~/.hushlogin
 
+# Packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 
