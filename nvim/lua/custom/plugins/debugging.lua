bf7f272351005d9327778797096d012bc62e628f
Tanguy Kervran <t.kervran29@gmail.com>
2024-07-09 01:41:02 +0200

    🐦 Add javascript debugging

--- nvim/lua/custom/plugins/debugging.lua
+++ nvim/lua/custom/plugins/debugging.lua
@@ -12,6 +12,26 @@ return {
     require('dapui').setup {}
     require('dap-go').setup {}
 
+    dap.adapters['pwa-node'] = {
+      type = 'server',
+      host = '127.0.0.1',
+      port = '${port}',
+      executable = {
+        command = 'js-debug-adapter',
+        args = { '${port}' },
+      },
+    }
+
+    dap.configurations.javascript = {
+      {
+        name = 'Launch file',
+        type = 'pwa-node',
+        request = 'launch',
+        program = '${file}',
+        cwd = vim.fn.getcwd(),
+      },
+    }
+
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end
@@ -27,5 +47,8 @@ return {
 
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Dap Toggle [B]reakpoint' })
     vim.keymap.set('n', '<F1>', dap.continue, { desc = 'Dap Continue' })
+    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Dap Step Over' })
+    vim.keymap.set('n', '<F3>', dap.step_into, { desc = 'Dap Step Into' })
+    vim.keymap.set('n', '<F4>', dap.step_out, { desc = 'Dap Step Out' })
   end,
 }
